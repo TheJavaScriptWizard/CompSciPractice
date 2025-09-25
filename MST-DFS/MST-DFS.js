8100532a4f6fc8f036d694d79459e1c4ab14b603
Garrison <robertwinters767@gmail.com>
2025-09-25 19:56:45 -0400

    I'm going to try out another methode to solve this problem

--- MST-DFS/MST-DFS.js
+++ MST-DFS/MST-DFS.js
@@ -1,83 +1,78 @@
 class Graph {
     
-    constructor(numOfNodes,nodes)
-    {
+    constructor(numOfNodes, nodes, edges) {
         this.numOfNodes = numOfNodes;
         this.nodes = nodes;
-        this.AdjList = new Map();      
+        this.AdjList = new Map();
         this.visitedNodes = Array(this.numOfNodes).fill(false);
-    }
+        this.edges = edges
+    } 
 
-    addNode(v)
-    {
+    addNode(v) {
         this.AdjList.set(v, []);
     }
 
-    addEdge(v, w)
-    {
+    addEdge(v, w) {
         this.AdjList.get(v).push(w);
         this.AdjList.get(w).push(v);
     }
 
-    printGraph()
-    {
+    printGraph() {
         var get_keys = this.AdjList.keys();
 
-        for (var i of get_keys)
-        {
+        for (var i of get_keys) {
             var get_values = this.AdjList.get(i);
             var conc = "";
 
-            for (var j of get_values)
-            {
+            for (var j of get_values) {
                 conc += j + " ";
             }
             console.log(i + " -> " + conc);
         }
     }
-    
-    DFS(currentNode)
-    {
+      Traversal(currentNode, acc) {
         if (this.visitedNodes[this.nodes.indexOf(currentNode)]) {
             return;
-        }else{
-            this.visitedNodes[this.nodes.indexOf(currentNode)] = true;
-            console.log(currentNode);
-            var neighbors = this.AdjList.get(currentNode);
-            for (let i = 0; i < neighbors.length; i++) {
-                this.DFS(neighbors[i]);
-            }  
         }
-    }
-
+        this.visitedNodes[this.nodes.indexOf(currentNode)] = true;
+        console.log(currentNode);
+        var neighbors = this.AdjList.get(currentNode);
+        for (let i = 0; i < neighbors.length; i++) {
+            this.Traversal(neighbors[i], this.edges.find(o => (o[0] = neighbors[i] && o[1] = currentNode) || (o[1] = neighbors[i] && o[0] = currentNode)))[0][2];
+        }  
+    }    
 }
 
-// Using the above implemented graph class
-var nodes = [ 'A', 'B', 'C', 'D', 'E', 'F' ];
-var g = new Graph(6,nodes);
+//Creating graph structure
+var edges = "A-B-10;A-D-7;A-E-8;B-C-6;D-E-2;E-F-8;C-E-1;C-F-7"
+    .split(";")
+    .map((x) => x.split("-"))
+    .map((arr) => [arr[0], arr[1], parseInt(arr[2], 10)]);
 
-// adding vertices
-for (var i = 0; i < nodes.length; i++) {
+var nodes = []
+
+for (var i = 0; i < edges.length;i++) {
+    if (nodes.includes(edges[i][0]) != true) {
+        nodes.push(edges[i][0]);
+    }else if (nodes.includes(edges[i][1]) != true) {
+        nodes.push(edges[i][1]);
+    }
+}
+
+nodes.sort();
+
+
+var g = new Graph(nodes.length, nodes, edges);
+
+//Fix, should probally use other exsiting for statment
+for (var i = 0;i < nodes.length;i++) {
     g.addNode(nodes[i]);
 }
 
-// adding edges
-g.addEdge('A', 'B');
-g.addEdge('A', 'D');
-g.addEdge('A', 'E');
-g.addEdge('B', 'C');
-g.addEdge('D', 'E');
-g.addEdge('E', 'F');
-g.addEdge('E', 'C');
-g.addEdge('C', 'F');
+// adding edges to g
+for (var i = 0; i < edges.length; i++) {
+    console.log(edges[i][0], edges[i][1]);
+    g.addEdge(edges[i][0], edges[i][1]);
+}
 
-// prints all vertex and
-// its adjacency list
-// A -> B D E
-// B -> A C
-// C -> B E F
-// D -> A E
-// E -> A D F C
-// F -> E C
-g.printGraph();
 g.DFS('A');
